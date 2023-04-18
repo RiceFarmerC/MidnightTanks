0bd353f25ace3f0b62c7ace5c46402b3655e9e8d
RiceFarmerC <dunxinchen@gmail.com>
2023-04-18 00:50:00 -0500

    turret rotation

--- Assets/horizontalAimScript.cs
+++ Assets/horizontalAimScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public class horizontalAimScript : MonoBehaviour
 {
-    public float rotationSpeed = 5f;
+    public float rotationSpeed;
     void Start()
     {
         
@@ -12,19 +12,21 @@ public class horizontalAimScript : MonoBehaviour
     
     void Update()
     {
+        // Get the position of the mouse on the screen
         Vector3 mouseScreenPosition = Input.mousePosition;
 
         // Step 2: Convert mouse cursor position to world coordinates
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, Camera.main.transform.position.z));
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, Camera.main.transform.position.y - transform.position.y));
 
         // Step 3: Calculate rotation angle
         Vector3 targetDirection = mouseWorldPosition - transform.position;
-        float rotationAngle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
+        float rotationAngle = Mathf.Atan2(targetDirection.x, targetDirection.z) * Mathf.Rad2Deg;
         float rotationStep = rotationSpeed * Time.deltaTime;
 
         // Step 4: Rotate towards the target position
-        float newAngle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, rotationAngle, rotationStep);
+        float newAngle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, rotationAngle, rotationStep);
         // Step 4: Apply rotation to the object
-        transform.rotation = Quaternion.Euler(new Vector3(-90f, 0f, rotationAngle));
+        transform.rotation = Quaternion.Euler(new Vector3(-90f, 90f, rotationAngle));
+
     }
 }
