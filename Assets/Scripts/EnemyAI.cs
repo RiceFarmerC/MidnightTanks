16775c9904ada3c569f8faa7ded082c9984db4b7
RiceFarmerC <dunxinchen@gmail.com>
2023-05-07 13:19:29 -0500

    Working AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -38,7 +38,7 @@ public class EnemyAI : MonoBehaviour
     private void Awake()
     {
         tankStats = GetComponent<TankStats>();
-        player = GameObject.Find("Player Tank").transform;
+        player = GameObject.Find("Player Tank with Turret Rotation").transform;
         agent = GetComponent<NavMeshAgent>();
         
     }
